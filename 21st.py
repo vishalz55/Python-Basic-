27a4a20c33b6ff3f2b8d360085d94bed892f87c8
Vishal Mungal <166546124+vishalz55@users.noreply.github.com>
2026-08-19 09:05:32 +0530

    Add files via upload

---
+++ 21st.py
@@ -0,0 +1,15 @@
+#elif smjl ka multiple condtion 
+
+
+a = 30
+b = 100 
+
+if a>b:
+    print("a is graetert tahan b")
+
+elif b>a:
+    print("b is graete than a")
+
+elif a==b:
+    print("a is === to b")
+    
---
+++ 22st.py
@@ -0,0 +1,11 @@
+a = 2025
+b = 2025 
+
+if a>b:
+    print("a is greate than b")
+
+elif b>a:
+    print("b is greate than a")
+
+else:
+    print("a is equal to b")
